7ccdaa5f07a820bc40bdad386db81706b02fbbd4
LeoDev <44649257+leodev12345@users.noreply.github.com>
2019-03-08 14:00:58 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+  var currentDate = new Date(),
+      day = currentDate.getDate(),
+      month = currentDate.getMonth() + 1,
+      year = currentDate.getFullYear();
+  document.write("Today is:" + " " + day + "." + month + "." + year + ".")
